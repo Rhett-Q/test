d99260e0db6e636ff9545c0a7fb52ac2bb53d125
qiurt <qiurt@sucsoft.com>
2020-08-16 18:26:27 +0800

    登录界面修改

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,26 +4,23 @@
       <div class="avatar_box">
         <img src="../assets/logo.png" />
       </div>
-      <el-form  label-width="0px">
+      <el-form label-width="0px" class="login_form">
         <el-form-item>
-            <el-input></el-input>
+          <el-input></el-input>
         </el-form-item>
         <el-form-item>
-            <el-input></el-input>
+          <el-input></el-input>
         </el-form-item>
-        <el-form-item>
-            <el-button type="primary">登录</el-button>
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
         </el-form-item>
-        <el-form-item>
-            <el-button type="info">重置</el-button>
-        </el-form-item>
-        </el-form>
+      </el-form>
     </div>
   </div>
 </template>
 <script>
-export default {
-}
+export default {}
 </script>
 <style lang="less" scoped>
 .login_container {
@@ -37,8 +34,10 @@ export default {
   width: 450px;
   height: 300px;
   background-color: white;
-  display: flex;
-  justify-content: center;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
 .avatar_box {
   height: 130px;
@@ -57,5 +56,16 @@ export default {
     border-radius: 50%;
     background-color: #eee;
   }
+}
+.login_form {
+    position: absolute;
+    width: 100%;
+    bottom:  0;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+.btns {
+    display: flex;
+    justify-content: flex-end;
 }
 </style>

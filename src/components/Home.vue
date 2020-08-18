59f8fb253f184333ef2bd8fecd772755b282809e
qiurt <qiurt@sucsoft.com>
2020-08-18 23:46:07 +0800

    首页布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,75 @@
 <template>
-    <div>home</div>
+  <el-container class="home-container">
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" />
+        <span>电商后台</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="gray"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item>
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>二级菜单</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 <style lang="less" scope>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: black;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: gray;
+}
+.el-main {
+  background-color: #eee;
+}
 </style>
